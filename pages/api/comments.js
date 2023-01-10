8824705db1bd02f5d74fe2ea80e9e91f2bcdbdfb
Sidhil sivadas M <pritamsidhill@gmail.com>
2023-01-10 09:36:13 +0530

    init 1

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,4 +1,13 @@
 import { comments } from "../../data/posts";
 export default function handler(req, res){
-    return res.status(200).json(comments)
+    if(req.method == "GET"){
+        return res.status(200).json(comments)
+    }else if(req.method == "POST"){
+        const comment = req.body.comment;
+        const data = {id: Date.now(), "title": comment, "details":"details - 1"}
+        comments.push(data);
+        return res.status(201).json(data)
+
+    }
+    
 }
--- pages/comments.js
+++ pages/comments.js
@@ -1,23 +1,56 @@
- import { useState } from "react";
+ import { useEffect, useState } from "react";
  function Comments(){
     const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => { 
+        // here is where you make API call(s) or any side effects
+        fetchComments()
+      }, [] ) /** passing empty brackets is necessary */
+
     const fetchComments = async () => {
+       setLoading(true)
        const response = await fetch(`api/comments`);
        const data = await response.json();
+       setLoading(false)
        setComments(data);
+       
     }
+
+    const addComment = async () => {
+       const response = await fetch(`api/comments`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({comment})
+       });
+       const data = await response.json();
+       console.log(data);
+       fetchComments();
+    }
+    
     return (
+        
         <>
-          <button onClick={fetchComments}>Fetch Comments</button>
+    
+          <input value={comment} onChange={(e) => setComment(e.target.value)}></input>
+          <button onClick={addComment}>Save</button>
+          <button onClick={fetchComments} >Fetch Comments</button>
+          <div className={ !loading ? `blink`: ''  }>
           {
             comments.map(comment => {
                 return(<div key={comment.id}>
-                    <p>{comment.title}</p>
+                    <p>{comment.id} | {comment.title}</p>
                 </div>)
             })
           }
+          </div>
         </>
     )
+
+    
  }
 
  export default Comments;
